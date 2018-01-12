8f5d91601411f58bac359914dc76301aaf3cddc0
Makayla Clausen <mclausen792@gmail.com>
2018-01-12 07:47:54 -0700

    Completed depth first search and breadth first search

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -35,12 +35,32 @@ class BinarySearchTree {
 	dfs(cb, node=this.root) {
     // Write an algorithm using depth first search that will take in a callback function and call it while passing each nodes data starting down the left path
 		// Expected order: '25, 15, 17, 197, 97, 9997'
+		let current = node
+
+		if(current === null){
+			return false;
+		}
+		cb(current.data)
+
+		this.dfs(cb, current.left)
+		this.dfs(cb, current.right)
 	}
 
 	bfs(cb, node=this.root) {
     // Write an algorithm using breadth first search that takes in a callback function and calls it for every node
     // Expected order: 25, 15, 197, 17, 97, 9997
-	}
-}
+
+		const q = []
+			q.push(node)
+			while (q.length > 0){
+			node = q.shift()
+			cb (node.data)
+			if (node.left)
+				 q.push(node.left)
+			if(node.right)
+				q.push(node.right)
+				}
+			}
+		}
 
 module.exports = BinarySearchTree
